ec038b206eaacb7c6b568109767c25c0c447321f
Felix Geyer <fel.geyer@gmail.com>
2023-03-28 10:26:04 -0700

    Resume training from checkpoint.
    Add a checkpoint after every episode and try to restore the checkpoint
    when starting training.  This allows to resume training after power
    outage, or continuing training from a checkpoint.

--- run_training.py
+++ run_training.py
@@ -4,18 +4,19 @@ Takes a training run configuration to simplify running experiments.
 
 Writes training curves to tensorboard to simplify comparisons between experiments.
 """
-from tqdm import tqdm
-import ml_collections
-import time
 import os
+import shutil
+import time
+from tqdm import tqdm
+
 import torch
 from torch import nn
+from torch import optim
 from torch.nn import functional as F
 from torchvision import datasets
 from torchvision import transforms
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-from torch import optim
 from configs.trial_config import trial_config
 
 
@@ -130,20 +131,35 @@ def load_dataset(root='~/datasets', batch_size=64):
 def train_with_config(config, base_dir=None):
     if base_dir is None:
         base_dir = './trials'
-    os.makedirs(os.path.join(base_dir, 'checkpoints'), exist_ok=True)
+    checkpoint_path = os.path.join(
+        base_dir, 'checkpoints', f'{config.trial_name}_latest.pt')
+    os.makedirs(os.path.dirname(checkpoint_path), exist_ok=True)
+
     writer = SummaryWriter(os.path.join(base_dir, 'runs', config.trial_name))
 
     train_data, test_data = load_dataset(
         '~/datasets', batch_size=config.batch_size)
 
-    # Select and configure the model.
+    # Initialize the training state (model, optimizer, epoch, step).
+    checkpoint = None
+    if os.path.exists(checkpoint_path):
+        checkpoint = torch.load(checkpoint_path)
+
     device = torch.device('cuda')
     if config.model_type == 'MLP':
         model = Mlp(dropout_rate=config.dropout_rate)
     elif config.model_type == 'ResNet':
         model = ResidualNet()
+
+    if checkpoint is not None:
+        model.load_state_dict(checkpoint['model_state_dict'])
     model = model.to(
         device, memory_format=torch.channels_last, non_blocking=True)
+
+    optimizer = optim.SGD(model.parameters(), lr=config.learning_rate,
+                          momentum=config.momentum, weight_decay=config.weight_decay)
+    if checkpoint is not None:
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
     with torch.no_grad():
         sample_batch, _ = next(iter(train_data))
@@ -151,28 +167,35 @@ def train_with_config(config, base_dir=None):
             device, memory_format=torch.channels_last))
 
     @torch.no_grad()
-    def evaluate_model(n_steps):
+    def evaluate_model(step):
         model.eval()
         losses = []
-        n_right, n_total = 0, 0
+        n_correct, n_total = 0, 0
         for batch, labels in test_data:
-            batch = batch.to(device, non_blocking=True)
+            batch = batch.to(
+                device, memory_format=torch.channels_last, non_blocking=True)
             labels = labels.to(device, non_blocking=True)
             logits = model(batch)
             preds = torch.argmax(torch.sigmoid(logits), dim=1)
-            n_right += torch.sum(preds == labels).item()
+            n_correct += torch.sum(preds == labels).item()
             n_total += batch.shape[0]
             loss = F.cross_entropy(logits, labels)
             losses.append(loss.item())
-        writer.add_scalar('Loss/eval', sum(losses) / len(losses), n_steps)
-        writer.add_scalar('Metrics/accuracy', n_right / n_total, n_steps)
+        writer.add_scalar('Loss/eval', sum(losses) /
+                          len(losses), step)
+        writer.add_scalar('Metrics/accuracy', n_correct /
+                          n_total, step)
 
-    optimizer = optim.SGD(model.parameters(), lr=config.learning_rate,
-                          momentum=config.momentum, weight_decay=config.weight_decay)
-    n_steps = 0
+    # Number of steps and epochs passed so far.
+    step, epoch = 0, 0
+    if checkpoint is not None:
+        step = checkpoint['step']
+        epoch = checkpoint['epoch']
+        print(f'Starting at epoch {epoch} at {step} steps.')
+
     # Run an evaluation on the initalized model.
-    evaluate_model(n_steps)
-    for epoch in tqdm(range(config.epochs)):
+    evaluate_model(step)
+    for epoch in tqdm(range(epoch + 1, config.epochs + 1)):
         model.train()
         for batch, labels in train_data:
             start = time.perf_counter_ns()
@@ -185,32 +208,36 @@ def train_with_config(config, base_dir=None):
             loss = F.cross_entropy(logits, labels)
             loss.backward()
             optimizer.step()
+            step += 1
 
             stop = time.perf_counter_ns()
             duration_ms = (stop - start) / 1e6
-            writer.add_scalar('Duration/step_ms', duration_ms, n_steps)
+            writer.add_scalar('Duration/step_ms', duration_ms, step)
             writer.add_scalar('Duration/example_ms',
-                              duration_ms / batch.shape[0], n_steps)
-            writer.add_scalar('Loss/train', loss.item(), n_steps)
+                              duration_ms / batch.shape[0], step)
+            writer.add_scalar('Loss/train', loss.item(), step)
             with torch.no_grad():
                 grad_norm = 0
                 for param in model.parameters():
                     grad_norm += torch.linalg.norm(param.grad.data).cpu() ** 2
                 grad_norm = torch.sqrt(grad_norm)
-            writer.add_scalar('grad_norm', grad_norm.item(), n_steps)
-            n_steps += 1
+            writer.add_scalar('grad_norm', grad_norm.item(), step)
 
+        # Save training state.
+        torch.save({
+            'epoch': epoch,
+            'step': step,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+        },
+            checkpoint_path)
         if epoch % config.checkpoint_after_episodes == 0:
-            torch.save({
-                'epoch': epoch,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-            },
-                os.path.join(base_dir, 'checkpoints',
-                             f'{config.trial_name}_epoch_{epoch}'))
+            shutil.copy(checkpoint_path,
+                        os.path.join(base_dir, 'checkpoints',
+                                     f'{config.trial_name}_epoch_{epoch}.pt'))
 
         # Evaluate on test data after each episode.
-        evaluate_model(n_steps)
+        evaluate_model(step)
 
 
 if __name__ == '__main__':
